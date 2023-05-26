76c110d369ad8f8fb0a3585514890840cf5147d8
Chris Rudolph <crudolph@omnitech-inc.com>
2023-05-26 09:53:44 -0600

    Part 2: Text trimming

--- src/components/MainStory/MainStory.js
+++ src/components/MainStory/MainStory.js
@@ -1,14 +1,9 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-const MainStory = ({
-  id,
-  title,
-  image,
-  location,
-  abstract,
-  ...delegated
-}) => {
+import { QUERIES } from "../../constants";
+
+const MainStory = ({ id, title, image, location, abstract, ...delegated }) => {
   return (
     <Wrapper {...delegated}>
       <a href={`/story/${id}`}>
@@ -44,6 +39,14 @@ const Abstract = styled.p`
   font-size: 1rem;
   margin-bottom: 1em;
   white-space: pre-wrap;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 8;
+
+  @media (${QUERIES.tabletAndUp}) {
+    -webkit-line-clamp: 16;
+  }
 `;
 
 const Location = styled.span`
